78dac7fcce5f405c67ef449ced7e691860884be8
Antidale <antidale@tellah.life>
2024-10-29 20:35:03 -0700

    more minor wip

--- Services/GrpcChannelService.cs
+++ Services/GrpcChannelService.cs
@@ -3,6 +3,9 @@ using Grpc.Net.Client.Web;
 
 namespace FF4FE.Tracker.Services
 {
+    /// <summary>
+    /// Gets a grpc channel at localhost:8190, SNI's default grpc web listen port
+    /// </summary>
     public static class GrpcChannelService
     {
         public static GrpcChannel? GetGrpcChannel() => GrpcChannel.ForAddress("http://localhost:8190", new GrpcChannelOptions { HttpHandler = new GrpcWebHandler(new HttpClientHandler()) });
