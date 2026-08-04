8f82195597e98b251a2755b16e0d378771851542
Antidale <antidale@tellah.life>
2026-08-04 15:19:52 -0700

    give sni time to become available. fix merge conflict

--- Models/SeedMetadata.cs
+++ Models/SeedMetadata.cs
@@ -2,18 +2,21 @@
 
 namespace FF4FE.Tracker.Models
 {
-    public class SeedMetadata
-    {
-        [JsonPropertyName("version")]
-        public string Version { get; set; } = string.Empty;
+        public class SeedMetadata
+        {
+                [JsonPropertyName("version")]
+                public string Version { get; set; } = string.Empty;
 
-        [JsonPropertyName("flags")]
-        public string Flags { get; set; } = string.Empty;
+                [JsonPropertyName("flags")]
+                public string Flags { get; set; } = string.Empty;
 
-        [JsonPropertyName("seed")]
-        public string Seed { get; set; } = string.Empty;
+                [JsonPropertyName("binary_flags")]
+                public string BinaryFlags { get; set; } = string.Empty;
 
-        // [JsonPropertyName("objectives")]
-        // public List<string> Objectives { get; set; } = new List<string>();
-    }
+                [JsonPropertyName("seed")]
+                public string Seed { get; set; } = string.Empty;
+
+                [JsonPropertyName("objectives")]
+                public string Objectives { get; set; } = string.Empty;
+        }
 }
