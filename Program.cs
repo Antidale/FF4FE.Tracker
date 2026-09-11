7cb5539468c8137d0bcf51e4e9131dee22d000fd
Antidale <antidale@tellah.life>
2026-09-11 13:01:28 -0700

    Large update - first pass nearly complete
    * using FeTracker.Common KI grid
    * started work on settings
    * split connection and metadata gathering
    * incorporated FeTracker.Common notifications for component messaging
    * Connection moved to header and uses plug icons for state
    * KI Count works
    * Reset Tracker keeps SNI connection but dumps any seed info, requiring a new Get Metadata button press

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
+using FeTracker.AutoTrack.Sni.Services;
+using FeTracker.Common.RazorComponents;
 using FF4FE.Tracker;
-using FF4FE.Tracker.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Grpc.Net.Client;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddSingleton<DeviceService>();
+builder.Services.AddSingleton<TrackerNotifier>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
